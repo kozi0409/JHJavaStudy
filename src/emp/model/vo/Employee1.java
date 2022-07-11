ecf4ccb580cd7b73f7815453956527fa846d0013
user <user@192.168.219.106>
2022-07-11 14:21:36 +0900

    first commit - 제목입니다..
    java day02 basic code - 내용입니다..
    한글로 써도 되고
    이것 저것 팀원들에게 무슨 작업을 했는지 알려주는 글을
    적어주면 좋습니다...
    구글에 커밋메시지 작성법을 찾아보시면 효율적으로 메시지를 작성하고 관리하는법을 알 수 있습니다..

---
+++ src/emp/model/vo/Employee1.java
@@ -0,0 +1,8 @@
+package emp.model.vo;
+
+public class Employee1 {
+	public void printEmployee() { // Employee 클래스가 가지고 있는 틀
+		System.out.println("저는 박지호입니다. 반갑습니다."); // 명령어
+		System.out.println("서울삽니다."); // 명령어
+	}
+}
